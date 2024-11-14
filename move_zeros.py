e72ff6888f53fe86818bb2301554888068be7fbf
Mostafa-Al-Bilani <mostafaalbilani@gmail.com>
2024-11-14 17:12:46 +0200

    commented the code

--- move_zeros.py
+++ move_zeros.py
@@ -1,8 +1,8 @@
 """a fucntion that moves 0s to the end of the list and maintains the order of the other elements"""
 numbers = [1, 2, 3, 4, 0, 0, 6, 0, 7, 0, 10, 123, 0, 12]
 def move_zeros(numbers):
-    counter = 0
-    i = 0
+    counter = 0 #counts number of zeros
+    i = 0 #counter to go through the list
     while i < len(numbers):
         if numbers[i] == 0:
             counter += 1
