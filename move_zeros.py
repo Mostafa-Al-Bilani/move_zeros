931378fb414ca7e780183fd9a0e03838845c5e91
Mostafa-Al-Bilani <mostafaalbilani@gmail.com>
2024-11-14 16:36:13 +0200

    created the function

--- move_zeros.py
+++ move_zeros.py
@@ -3,3 +3,13 @@ numbers = [1, 2, 3, 4, 0, 0, 6, 0, 7, 0, 10, 123, 0, 12]
 def move_zeros(numbers):
     counter = 0
     i = 0
+    while i < len(numbers):
+        if numbers[i] == 0:
+            counter += 1
+            numbers.pop(i)
+        else:
+            i += 1
+
+    numbers.extend([0] * counter)
+    return numbers
+print(move_zeros(numbers))
